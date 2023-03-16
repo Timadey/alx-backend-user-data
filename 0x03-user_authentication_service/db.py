4ce6aa17b27ca81552ad3cdd96a7904b223c33fd
Timadey <dlktimothy@gmail.com>
2023-03-16 20:15:03 +0100

    Define update_user method

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,11 +1,14 @@
+#!/usr/bin/env python3
 """DB module
 """
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
-from user import Base
+from user import Base, User
 
 
 class DB:
@@ -28,3 +31,35 @@ class DB:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """Save a user to the database"""
+        auser = User(email=email, hashed_password=hashed_password)
+        self._session.add(auser)
+        self._session.commit()
+        return auser
+
+    def find_user_by(self, **kwargs: dict) -> User:
+        """Returns the first row found in the user tables as filtered
+        by the method's input arguments
+        """
+        try:
+            auser = self._session.query(User).filter_by(**kwargs).one()
+        except NoResultFound:
+            raise
+        except InvalidRequestError:
+            raise
+        return auser
+
+    def update_user(self, user_id: int, **kwargs: dict) -> None:
+        """Uses find_user_by to locate the user to update then update
+        the user's attribute as passed in the method's arguments
+        """
+        try:
+            auser = self.find_user_by(id=user_id)
+            for k, v in kwargs.items():
+                auser.__setattr__(k, v)
+            self.__session.add(auser)
+            self.__session.commit()
+        except ValueError:
+            raise
--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,9 +1,12 @@
-#!/usr/bin env/python3
+#!/usr/bin/env python3
 """A user Uer model"""
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
 
 
-class User():
+class User(Base):
     """A user model with the attributes
     id: integer peimary key
     email: a non nullable string
@@ -11,7 +14,9 @@ class User():
     session_id: a nullable string
     reset_token: a nullable string
     """
-    id = Column(Integer, primary_key=True)
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
