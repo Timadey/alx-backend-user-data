b587f49f40550b19b494bb6eaa7d0806b63a4c33
Timadey <dlktimothy@gmail.com>
2023-03-16 19:24:15 +0100

    create user model

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env/python3
+"""A user Uer model"""
+from sqlalchemy import Column, Integer, String
+
+
+class User():
+    """A user model with the attributes
+    id: integer peimary key
+    email: a non nullable string
+    hashed_password: a non nullable string
+    session_id: a nullable string
+    reset_token: a nullable string
+    """
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
